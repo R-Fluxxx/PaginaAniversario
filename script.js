5a4c12741bf4d7a6f02854187f00b224be64d6e2
Jefferson Souza <110081786+R-Fluxxx@users.noreply.github.com>
2024-02-06 20:43:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Inicializa a biblioteca ScrollReveal
+ScrollReveal().reveal('section', {
+    delay: 200,
+    distance: '50px',
+    easing: 'ease-in-out',
+    origin: 'bottom',
+    duration: 1000
+});
